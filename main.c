1afdf92faf33956b7fe196b74d1e4f6828941e66
Henrique Bissoli Malaman Alonso <bissoli@3stamina.com>
2023-03-08 18:08:31 -0300

    Added function to sort using insertionSort method

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ int n; //Tamanho do Array
 int *arrayOriginal; //Array gerado automaticamente e sem alteranção
 
 void geraArrayAleatorio(int arrayOriginal[], int n);
+void imprimeArray(int arrayOriginal[], int n);
+void insertionSort(int arrayOrigina[], int n);
 
 int main() {
     printf("\tProjeto de Algoritmos para ordenação\n");
@@ -18,6 +20,16 @@ int main() {
 
     //Gerando número aleatórios para o Array Original
     geraArrayAleatorio(arrayOriginal, n);
+
+    //imprimindo o Array Original
+    printf("Array Original[%d]:", n);
+    imprimeArray(arrayOriginal, n);
+
+    //Ordenado pelo Método de Insertion Sort
+    insertionSort(arrayOriginal, n);
+    printf("\nArray Ordenado[%d]:", n);
+    imprimeArray(arrayOriginal, n);
+
     return 0;
 }
 /* Função geraArrayAleatorio() define números aleatórios de 0 a 100 para um array específico */
@@ -26,5 +38,27 @@ void geraArrayAleatorio(int arrayOriginal[], int n){
 
     for(int i = 0; i < n; i++){
         arrayOriginal[i] = rand() % 100 + 1;
+    }
+}
+
+/* Função imprimeArray(): imprime os n elementos de um array específico */
+void imprimeArray(int arrayOriginal[], int n){
+    for(int i = 0; i < n; i++){
+        printf(" %d", arrayOriginal[i]);
+    }
+}
+
+/* Função insertionSort(): ordena o array específico aplicando o método de ordenação por inserção */
+void insertionSort(int arrayOriginal[], int n){
+    int i, j, chave;
+
+    for(j = 1; j < n; j++){
+        chave = arrayOriginal[j];
+        i = j - 1;
+        while((i >= 0) && (arrayOriginal[i] > chave)){
+            arrayOriginal[(i + 1)] = arrayOriginal[i];
+            i--;
+        }
+        arrayOriginal[(i + 1)] = chave;
     }
 }
